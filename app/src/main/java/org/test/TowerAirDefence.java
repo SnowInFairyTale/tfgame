14df1a7441a292379e1daf36857cb41f3c2de913
zhipengguo <snow1234>
2021-08-21 01:15:16 +0800

    ..

--- app/src/main/java/org/test/TowerAirDefence.java
+++ app/src/main/java/org/test/TowerAirDefence.java
@@ -5,7 +5,13 @@ public class TowerAirDefence extends Tower
 	public TowerAirDefence(MainGame game)
 	{
 		super(game, TowerType.AirDefence, Capability.Air, "assets/towers/air_tower.png");
-		super.setTowerLevels(new TowerLevel[] {new TowerLevel(10, 70f, 10, 0.5f, 0.2f, 0f), new TowerLevel(10, 80f, 0x12, 0.5f, 0.2f, 5f), new TowerLevel(15, 90f, 0x19, 0.5f, 0.2f, 5f), new TowerLevel(20, 100f, 0x23, 0.5f, 0.2f, 5f), new TowerLevel(0x19, 100f, 50, 0.5f, 0.2f, 10f)});
+		super.setTowerLevels(new TowerLevel[] {
+				new TowerLevel(10, 70f, 20, 0.25f, 0.2f, 0f),
+				new TowerLevel(10, 80f, 0x24, 0.25f, 0.2f, 5f),
+				new TowerLevel(15, 90f, 0x38, 0.25f, 0.2f, 5f),
+				new TowerLevel(20, 100f, 0x46, 0.25f, 0.2f, 5f),
+				new TowerLevel(0x19, 100f, 100, 0.25f, 0.2f, 5f)
+		});
 		super.SetValuesFromTowerLevel(0);
 		super.SetInitialValue();
 	}
--- app/src/main/java/org/test/TowerAxe.java
+++ app/src/main/java/org/test/TowerAxe.java
@@ -5,7 +5,13 @@ public class TowerAxe extends Tower
 	public TowerAxe(MainGame game)
 	{
 		super(game, TowerType.Axe, Capability.AirGround, "assets/towers/bash_tower.png");
-		super.setTowerLevels(new TowerLevel[] {new TowerLevel(5, 60f, 10, 1.5f, 0.6f, 0f), new TowerLevel(5, 60f, 20, 1.5f, 0.6f, 5f), new TowerLevel(10, 70f, 0x23, 1.5f, 0.6f, 8f), new TowerLevel(30, 70f, 60, 1.5f, 0.6f, 12f), new TowerLevel(70, 90f, 80, 1.5f, 0.6f, 15f)});
+		super.setTowerLevels(new TowerLevel[] {
+				new TowerLevel(5, 60f, 20, 0.7f, 0.6f, 0f),
+				new TowerLevel(5, 60f, 40, 0.7f, 0.6f, 5f),
+				new TowerLevel(10, 70f, 0x46, 0.7f, 0.6f, 5f),
+				new TowerLevel(30, 70f, 120, 0.7f, 0.6f, 5f),
+				new TowerLevel(70, 90f, 160, 0.7f, 0.6f, 5f)
+		});
 		super.SetValuesFromTowerLevel(0);
 		super.SetInitialValue();
 	}
