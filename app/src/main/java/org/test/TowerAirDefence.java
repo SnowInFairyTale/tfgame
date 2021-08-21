0fa3cc1b4b694b93749d557b4919f0814ecb3982
zhipengguo <snow1234>
2021-08-21 14:13:11 +0800

    ..

--- app/src/main/java/org/test/TowerAirDefence.java
+++ app/src/main/java/org/test/TowerAirDefence.java
@@ -4,7 +4,7 @@ public class TowerAirDefence extends Tower
 {
 	public TowerAirDefence(MainGame game)
 	{
-		super(game, TowerType.AirDefence, Capability.Air, "assets/towers/air_tower.png");
+		super(game, TowerType.AirDefence, Capability.AirGround, "assets/towers/air_tower.png");
 		super.setTowerLevels(new TowerLevel[] {
 				new TowerLevel(10, 70f, 20, 0.25f, 0.2f, 0f),
 				new TowerLevel(10, 80f, 0x24, 0.25f, 0.2f, 5f),
